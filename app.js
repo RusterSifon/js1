be82f789e3bcd696ef31b27971b6272fbff6f853
RusterSifon <muhitdinovramil0@gmail.com>
2024-09-13 08:58:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+document.write('sadfsfa')
+console.log('wertea')
+//console.log(5 + 6)
+//console.log(14 + 88)
+let mouth = 30
+let hour = 24
+let minut = 60
+let sec = 60
+console.log(`в 30 сутках ${mouth*hour*minut*sec} секунд`)
+
+let year = 365
+let hours = 24
+let minuts = 60
+let second = 60
+console.log(`в году ${year*hours*minuts*second} секунд`)
+
+hours = 24
+minuts = 60
+console.log(`в сутках ${hours*minuts} минут`)
+
+year = 365
+hours = 24
+minuts = 60
+console.log(`в году ${year*hours*minuts} минут`)
+
+let byte = 8 bit
+let kilobyte = 1024 byte
+
+
+
