06f5cab9cc98f5f6783e89d32eeb956c9f0c4a33
RusterSifon <muhitdinovramil0@gmail.com>
2024-09-19 15:01:25 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ document.write('sadfsfa')
 console.log('wertea')
 //console.log(5 + 6)
 //console.log(14 + 88)
-let mouth = 30
+
 let hour = 24
 let minut = 60
 let sec = 60
+console.log(`в 1 сутках ${mouth*hour*minut*sec} секунд`)
+
+let mouth1 = 30
+let hour1 = 24
+let minut2 = 60
+let sec1 = 60
 console.log(`в 30 сутках ${mouth*hour*minut*sec} секунд`)
 
 let year = 365
@@ -23,8 +29,24 @@ hours = 24
 minuts = 60
 console.log(`в году ${year*hours*minuts} минут`)
 
-let byte = 8 bit
-let kilobyte = 1024 byte
-
-
-
+let year = 365
+let day = 30
+let hour = 24
+let minute = 60
+let sec = 60
+console.log(в сутках ${hour*minute*sec} секунд)
+console.log(в 30 сутках ${days*hour*minute*sec} секунд)
+console.log(в году ${year*hour*minute*sec} секунд)
+console.log(в сутках ${hour*minute} минут)
+console.log(в году ${year*hour*minute} минут)
+let onekb = 1
+let кб = 1024
+let мб = 1024
+let гб = 1024
+let tengb = 1024*10
+let tb = 1024
+console.log(в мегабайте ${bait*kb*mb} байт)
+console.log(в гигабайте ${bait*kb*mb*gb} байт)
+console.log(в 10 гигабайтах ${bait*kb*mb*tengb} байт)
+console.log(в терабайте ${bait*kb*mb*gb*tb} байт)
+console.log(в терабайте ${onekb*mb*gb*tb} килобайт)
